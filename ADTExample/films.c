0fd1a0ccad7d6ee47b3264d81f92df39fc57d4c0
pengliang.wang <pengliang.wang@xundayuncheng.com>
2015-12-21 15:58:29 +0800

    ADTExample

---
+++ ADTExample/films.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#include "list.h"
+
+void showmovies(Item item);
+
+int main(void) {
+	List movies;
+	Item temp;
+	
+	//initialize
+	InitializeList(&movies);
+	if(ListIsFull(movies)) {
+		fprintf(stderr,"No memory available!\n");
+		exit(1);
+	}
+
+	//collect and save
+	puts("Enter first movie title:");
+	while(gets(temp.title) != NULL && temp.title[0] != '\0') { 
+		puts("Enter your rating<0-10>:");
+		scanf("%d",&temp.rating);
+		while(getchar() != '\n')
+			continue;
+		if(AddItem(temp,&movies) == false) {
+			fprintf(stderr,"Problem allocating memory.\n");
+			break;
+		}
+		if(ListIsFull(movies)) {
+			puts("The list is now full.");
+			break;
+		}
+		puts("Enter next movie title:");
+	}
+
+	//show
+	if(ListIsEmpty(movies))
+		printf("No data entered.\n");
+	else {
+		printf("Here is the movie list: \n");
+		Traverse(movies,showmovies);
+	}
+	printf("You entered %d movies.\n",ListItemCount(movies));
+
+	//clean
+	EmptyTheList(&movies);
+	printf("Bye\n");
+	return 0;
+}
+
+void showmovies(Item item) {
+	printf("Movie:%s Rating:%d\n",item.title,item.rating);
+}
